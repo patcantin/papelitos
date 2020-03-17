9c0b39c384d716ddcb905f794d10e5f968a3a01d
patcantin <patcantin23@gmail.com>
2020-03-17 18:35:04 -0400

    modification to components/button, starting of key creation in games view/model/controller

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,9 +1,21 @@
 class GamesController < ApplicationController
   def index
     @user = current_user
-  end
-  def new
+    @game = Game.new
   end
   def create
+    @game = Game.new(game_params)
+    @game.user = current_user
+    if @game.save!
+      redirect_to index_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def game_params
+    params.require(:game).permit(:key_number, :user_id)
   end
 end
--- app/javascript/packs/landing.js
+++ app/javascript/packs/landing.js
@@ -1,13 +1,13 @@
 
-  const create = document.querySelector(".create");
-  const join = document.querySelector(".join");
+  // const create = document.querySelector(".create");
+  // const join = document.querySelector(".join");
 
-  create.addEventListener('click', (event) => {
-    const showKey = document.querySelector(".key");
-    showKey.insertAdjacentHTML('beforeend', 8465);
-  });
+  // create.addEventListener('click', (event) => {
+  //   const showKey = document.querySelector(".key");
+  //   showKey.insertAdjacentHTML('beforeend', 1234);
+  // });
 
-  join.addEventListener('click', (event) => {
-    const showInput = document.querySelector(".enter_key");
-    showInput.classList.remove('rem');
-  });
+  // join.addEventListener('click', (event) => {
+  //   const showInput = document.querySelector(".enter_key");
+  //   showInput.classList.remove('rem');
+  // });
