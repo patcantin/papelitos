3e4c1dc9320f41fb2e747ee02059916741a67a24
patcantin <patcantin23@gmail.com>
2020-03-17 21:25:52 -0400

    key_number appears to new_game_words page, new redirection for create in game controller

--- app/controllers/game_words_controller.rb
+++ app/controllers/game_words_controller.rb
@@ -1,2 +1,13 @@
 class GameWordsController < ApplicationController
+  before_action :set_words, only: [:new]
+  def new
+  end
+  def create
+  end
+
+  private
+
+  def set_words
+    @game = Game.find(params[:game_id])
+  end
 end
