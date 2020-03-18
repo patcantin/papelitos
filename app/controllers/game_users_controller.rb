571bd7ba9371e0d3ecfcf22f8cb0753bc16e8d24
patcantin <patcantin23@gmail.com>
2020-03-18 17:15:07 -0400

    game_users controller + game_user new view

--- app/controllers/game_users_controller.rb
+++ app/controllers/game_users_controller.rb
@@ -1,2 +1,28 @@
 class GameUsersController < ApplicationController
+  before_action :set_game, only: [:new]
+  def new
+    @games = Game.all
+    @game_user = GameUser.new
+  end
+  def create
+    @game_user = GameUser.new(game_user_params)
+    @game = Game.find(params[:game_id])
+    @game_user.game = @game
+    @game_user.user = current_user
+    if @game_user.save!
+      redirect_to new_game_game_word_path(@game.id)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def set_game
+    @game = Game.find(params[:game_id])
+  end
+
+  def game_user_params
+    params.require(:game_user).permit(:game_id, :user_id)
+  end
 end
