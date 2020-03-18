33ab4556e1c5ebb2dad969f731a07a8e8234cfa6
patcantin <patcantin23@gmail.com>
2020-03-17 20:08:34 -0400

    key generated to game table

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -4,18 +4,12 @@ class GamesController < ApplicationController
     @game = Game.new
   end
   def create
-    @game = Game.new(game_params)
+    @game = Game.new
     @game.user = current_user
     if @game.save!
-      redirect_to index_path
+      redirect_to games_path
     else
       render :index
     end
-  end
-
-  private
-
-  def game_params
-    params.require(:game).permit(:key_number, :user_id)
   end
 end
