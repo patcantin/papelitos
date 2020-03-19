687261e5da63ff2a2eaec248f1bdb6ea7d83bdb3
patcantin <patcantin23@gmail.com>
2020-03-19 19:35:00 -0400

    empty change

--- app/controllers/game_users_controller.rb
+++ app/controllers/game_users_controller.rb
@@ -1,10 +1,4 @@
 class GameUsersController < ApplicationController
-
-  # def create
-  #   @game = Game.find_by_key_number(params[:]) # put the right params here
-  #   redirect_to games_path(game)
-  # end
-
   before_action :set_game, only: [:new]
   def new
     @games = Game.all
