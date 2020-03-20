d3fd9fa9ab0975017646456a984b80c377ce40c0
patcantin <patcantin23@gmail.com>
2020-03-19 21:23:03 -0400

    create user by join

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,15 +1,34 @@
 class GamesController < ApplicationController
+  before_action :set_game, only: [:new]
   def index
     @user = current_user
     @game = Game.new
+    @game_user = GameUser.new
   end
   def create
     @game = Game.new
     @game.user = current_user
     if @game.save!
-      redirect_to new_game_game_user_path(@game.id)
+      @game_user = GameUser.new(game_user_params)
+      @game_user.game = @game
+      @game_user.user = current_user
+      if @game_user.save!
+        redirect_to new_game_game_word_path(@game.id)
+      else
+        render :index
+      end
     else
       render :index
     end
+  end
+
+  private
+
+  def set_game
+    @game = Game.find(params[:game_id])
+  end
+
+  def game_user_params
+    params.permit(:game_id, :user_id)
   end
 end
