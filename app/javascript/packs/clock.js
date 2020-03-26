cd783a124676d89b3fe91a5f2c5b0b3bd0aeaa81
patcantin <patcantin23@gmail.com>
2020-03-26 08:24:50 -0400

    Show first word and first player on games/show + _timer.scss & clock.js to show timer

--- app/javascript/packs/clock.js
+++ app/javascript/packs/clock.js
@@ -1,7 +1,27 @@
-    var timeleft = 60;
-    var downloadTimer = setInterval(function(){
-    timeleft--;
-    document.getElementById("countdowntimer").textContent = timeleft;
-    if(timeleft <= 0)
-        clearInterval(downloadTimer);
-    },1000);
+var interval;
+
+function countdown() {
+  clearInterval(interval);
+  interval = setInterval( function() {
+      var timer = $('.js-timeout').html();
+      timer = timer.split(':');
+      var seconds = timer[0];
+      seconds -= 1;
+if (seconds < 10 && length.seconds != 2) seconds = '0' + seconds;
+
+      $('.js-timeout').html(seconds);
+
+      if (seconds == 0) clearInterval(interval);
+  }, 900);
+}
+
+$('#js-startTimer').click(function () {
+  $('.js-timeout').text("60");
+  countdown();
+});
+
+
+// $('#js-resetTimer').click(function () {
+//   $('.js-timeout').text("60");
+//   clearInterval(interval);
+// });
