e1a4b89e4ae8558dff246f4fcdd6664b67ea4948
Morgane Dinse <morgane.dinse@gmail.com>
2020-03-28 15:32:22 -0400

    created meta.yml, added the code; did helpers set-up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,3 +11,8 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:account_update, keys: [:username])
   end
 end
+
+# app/controllers/application_controller.rb
+def default_url_options
+  { host: ENV["DOMAIN"] || "localhost:3000" }
+end
