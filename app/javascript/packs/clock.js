7489863208b5c909d7fbe9454a55103b02ff6ed0
patcantin <patcantin23@gmail.com>
2020-03-31 20:13:22 -0400

    global models-controllers refactored

--- app/javascript/packs/clock.js
+++ app/javascript/packs/clock.js
@@ -1,22 +1,22 @@
-var interval;
+// var interval;
 
-function countdown() {
-  clearInterval(interval);
-  interval = setInterval( function() {
-      var timer = $('.js-timeout').html();
-      timer = timer.split(':');
-      var seconds = timer[0];
-      seconds -= 1;
-if (seconds < 10 && length.seconds != 2) seconds = '0' + seconds;
+// function countdown() {
+//   clearInterval(interval);
+//   interval = setInterval( function() {
+//       var timer = $('.js-timeout').html();
+//       timer = timer.split(':');
+//       var seconds = timer[0];
+//       seconds -= 1;
+// if (seconds < 10 && length.seconds != 2) seconds = '0' + seconds;
 
-      $('.js-timeout').html(seconds);
+//       $('.js-timeout').html(seconds);
 
-      if (seconds == 0) clearInterval(interval);
-  }, 900);
-}
+//       if (seconds == 0) clearInterval(interval);
+//   }, 900);
+// }
 
-  $('.js-timeout').text("60");
-  countdown();
+//   $('.js-timeout').text("60");
+//   countdown();
 
 
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_one_attached :photo
-  has_many :game_users
+  has_many :game_users, dependent: :destroy
   has_many :game_teams, through: :game_users
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
