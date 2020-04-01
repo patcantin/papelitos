4e165e0bc08719b41b16d3384c8d24f471b59db5
Morgane Dinse <morgane.dinse@gmail.com>
2020-03-31 20:38:12 -0400

    created a start button

--- db/migrate/20200330164847_remove_game_user_id_from_games.rb
+++ db/migrate/20200330164847_remove_game_user_id_from_games.rb
@@ -1,5 +1,5 @@
 class RemoveGameUserIdFromGames < ActiveRecord::Migration[6.0]
-  def change
-    remove_reference :games, :game_user, null: false, foreign_key: true
-  end
+  # def change
+  #   remove_reference :games, :game_user, null: false, foreign_key: true
+  # end
 end
