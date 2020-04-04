02cd257c40bd20a2bcbd62eaa6aa32c1b83361ac
Morgane Dinse <morgane.dinse@gmail.com>
2020-04-04 10:43:10 -0400

    added javascript to footer in application.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,3 +3,14 @@ require("@rails/activestorage").start()
 require("channels")
 
 import "bootstrap";
+
+// for footer
+if(/Android [4-6]/.test(navigator.appVersion)) {
+   window.addEventListener("resize", function() {
+      if(document.activeElement.tagName=="INPUT" || document.activeElement.tagName=="TEXTAREA") {
+         window.setTimeout(function() {
+            document.activeElement.scrollIntoViewIfNeeded();
+         },0);
+      }
+   })
+}
